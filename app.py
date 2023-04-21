40fada8256c4d5273919de3acc5b831442078578
Pasinee <pasinee.sb@gmail.com>
2023-04-21 05:31:31 -0400

    add os for env vars

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User, Feedback
 from forms import RegisterForm, LogInForm, FeedbackForm
 from sqlalchemy.exc import IntegrityError
+import os
 
 app = Flask(__name__)
 app.app_context().push()
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth_exercise"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = "abc123"
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY', 'hello!123')
+print('###############')
+print(app.config["SECRET_KEY"])
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 
